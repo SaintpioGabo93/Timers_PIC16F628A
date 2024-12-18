577600025e3a64a7187ab469328ad9192338a418
Saintpiogabo93 <tocata_fuga_dorian@hotmail.com>
2024-12-18 16:34:41 -0600

    Se termino con el codigo ya compilado

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Timers_PIC16F628A
 Repositorio para ejercicio de codigo con Timers en ensamblador para el PIC16F628A
+
+256x4 -> Este es porque vamos a multiplicar nuestros 4Mhz por 256, que es poner los bits PS3 = 1. PS2 = 1, PS0 = 1
+
+
+Vamos a poner el ejemplo con los bits del registro TMR0
+Los bits 7 y 6, no nos interesan, por lo que su valor va a ser 0
+RBPU = 0
+INTEDEG = 0
+
+Recordemos que el bit 5 es para determinar si queremos que sea un timer = 0, o un contador = 1. 
+Por lo que, como queremos que sea timer, lo ponemos en 0
+TOCS = 0
+
+El bit 4, es cuando lo configuramos como contador, por lo que no nos interesa
+TOSE = 0
+
+El bit 3, es para elegir en que timer vamos a usar el prescalar, si en TMR0 = 0, o en WDT = 1
+Como lo queremos en el TMR; entonces:
+PSA = 0
+
+Y los ultimos tres bits, son los bits escalares. 
+PS2 = 1
+PS1 = 1
+PS0 = 1
+
+
+
+RBPU	INTEDEG	TOCS	TOSE	PSA	PS2	PS1	PS0
+0	  0	 0	 0	 0	 1	 1	 1
+
+Con esto tenemos que el TMR0 se aumenta cada 256 ciclos de instrucción, o sea 256 x 1 microsegundo	
+
+
+Vamos a hacer las matemáticas: 
+
+TMR0 
+
+00000000 -> Cuando ponemos el TMR0 a cero
+00000001 -> Despues de 256 microsegundos
+00000010 -> Despues de otros 256 microsegundos
+00000011 -> Despues de otros 256 microsegundos
+00000100 -> Este es el bit que vamos a testear hasta que de 4, asi pasa despues de que pasaron 256 microsegundos x 4 veces que hubo ciclos de reloj
+
+Así 256micros + 256micros + 256micros + 256micros = 1024 microsegundo
+
+
+
--- Timers_PIC.asm
+++ Timers_PIC.asm
@@ -1,25 +1,50 @@
 ; CONFIG
 ; __config 0xFF59
  __CONFIG _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _BOREN_ON & _LVP_OFF & _CPD_OFF & _CP_OFF
-    
+
+;Configuracion de variables para el codigo
+    temp1 equ 0x20 ; Creamos una variable de nombre temp1 en la direccion 0x20 de la memoria de datos
+
+;Configuracion del pic y todas sus palabras claves
     list p=16f628a
     #include "/opt/microchip/mplabx/v5.20/mpasmx/p16lf628a.inc"
+
+;Iniciamos el programa
     org 0x00
-    
-    ; Iniciamos nuestro programa
-    bsf		status, 5   ; vamos al banco 1
-    clrf	trisb	    ; Ponemos todos los bits del trisb en 0, o sea, que estan configuradas como salidas. 
-    bcf		status, 5   ; Regresamos al banco 0
-    
-    ; Encedemos el LED
-    bsf		portb, 0    ; El bit 0 del PORTB, queda en HIGH
-    movlw	.100	    ; Cargamos el numero 100 al registro w
-    call	delay_ms    ; Llamamos a la funcion delay_ms para que se tarde la cantidad de tiempo que 
-    
-    ; Apagamos el LED
-    bcf		portb, 0    ; El bit 0 del PORTB, queda en LOW
-    movlw	.100	    ; Cargamos el numero 100 al registro w
-    call	delay_ms    ; Llamamos a la funcion de retardo
-    goto	$-6	    ; Regregamos a bsf portb, 0, o sea a hig :v
-    
+
+; Iniciamos nuestro programa
+    bsf     STATUS, 5   ; vamos al banco 1
+    clrf    TRISB      ; Ponemos todos los bits del TRISB en 0, o sea, que están configuradas como salidas.
+    movlw   b'00000111' ; Cargamos todos los bits como determinamos en el archivo readme para que el prescaler sea 256 en modo timer
+    movwf   OPTION_REG  ; El valor que cargamos en la instrucción anterior, lo escribimos en el OPTION_REG
+    bcf     STATUS, 5   ; Regresamos al banco 0
+
+; Encedemos el LED
+    bsf     PORTB, 0    ; El bit 0 del PORTB, queda en HIGH
+    movlw   .100        ; Cargamos el número 100 al registro W
+    call    delay_ms    ; Llamamos a la función delay_ms para que se tarde la cantidad de tiempo que
+
+; Apagamos el LED
+    bcf     PORTB, 0    ; El bit 0 del PORTB, queda en LOW
+    movlw   .100        ; Cargamos el número 100 al registro W
+    call    delay_ms    ; Llamamos a la función de retardo
+    goto    $-6         ; Regresamos a bsf PORTB, 0, o sea a HIGH
+
+; Parte de la función para que el bucle del milisegundo se ejecute 100 veces
+delay_ms:
+    movwf   temp1      ; Movemos la información que teníamos cargada en movlw a la variable temp1
+    call    delay_1ms  ; Vamos a la función de 1 milisegundo
+    decfsz  temp1, 1   ; Si el destino (1) vale 0, el valor se asigna a W, si el destino(1) vale 1, entonces temp1 = temp1 - 1, y si temp1 - 1 = 0 entonces salta una instrucción
+    goto    $-2        ; Si la instrucción anterior no fue 0, llega a esta instrucción para regresar a la función delay_1ms 
+    return
+
+; Creamos el bucle para contar el milisegundo
+delay_1ms:
+    clrf    TMR0      ; Ponemos el TMR0, todo a cero
+    btfss   TMR0, 2   ; Testeamos el bit 2 hasta que dé 1, recordemos que como ya configuramos el OPTION_REG en sus últimos 3 bits a uno, ya se va a tardar 256 microsegundos
+    goto    $-1       ; En caso de que no sea 1 el bit anterior, llegará a esta instrucción y regresará a la instrucción anterior hasta que se cumpla la condición
+    return            ; Esto ocurre cuando llega al valor, porque se salta una instrucción con la instrucción btfss
     end
+
+    
+    
